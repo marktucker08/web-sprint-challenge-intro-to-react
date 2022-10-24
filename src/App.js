a782dd9d6570a62031d1b2ad7a410cb370e0b1d7
Mark Tucker <markdaddy@Marks-MacBook-Air.local>
2022-10-23 19:26:08 -0600

    basic character component created and get request

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import styled from 'styled-components';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data, setData] = useState([]);
+  const [currentId, setCurrtentId] = useState(null);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people/')
+      .then(res => {
+        // console.log(res.data);
+        setData(res.data);
+      }).catch(err => console.log(err));
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+
+         {data.map((char) => { 
+          return <Character info={char} />
+        })
+      }
+      {
+        //  currentId && <Details friendId={currentFriendId} close={closeDetails} />
+      }
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import styled from "styled-components";
+import React from 'react'
+
+export default function Character({ info }) {
+  return (
+    <div className='character'>
+      {info.name}
+      <button>
+        See details
+      </button>
+    </div>
+  )
+}
